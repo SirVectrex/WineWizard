dc85b4ff7b559d1b45c329da064c0ec83e8425e3
SirVectrex <florian.schulenberg@web.de>
2023-11-28 22:40:10 +0200

    add header and footer, add sample pages for wine content, add sample router without business logic for sample pages

---
+++ src/main/java/com/winewizard/winewizard/controller/GeneralController.java
@@ -0,0 +1,27 @@
+package com.winewizard.winewizard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GeneralController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(method= RequestMethod.GET, value = "/statistics")
+    public String statistics() {
+        // folder general/statistics.html
+        return "general/statistics";
+    }
+
+    @RequestMapping(value = "/feedback", method = RequestMethod.GET)
+    public String feedback() {
+        // folder general/feedback.html
+        return "general/feedback";
+    }
+
+}
--- src/main/java/com/winewizard/winewizard/controller/WineController.java
+++ src/main/java/com/winewizard/winewizard/controller/WineController.java
@@ -7,9 +7,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class WineController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String home() {
-        return "home";
+    @RequestMapping(method = RequestMethod.GET, value = "/addRating")
+    public String addRating() {
+        return "wines/rate_wine";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/showAll")
+    public String showAll() {
+        return "wines/all_wines";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add_Description")
+    public String add_Description() {
+        return "wines/add_Description";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/sendAll")
+    public String sendAll() {
+        return "wines/send_all_wines";
     }
 
 }
