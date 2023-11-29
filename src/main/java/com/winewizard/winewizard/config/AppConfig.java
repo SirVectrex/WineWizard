c57c1d9f77cf951a58c181858f9ee188cc23acb7
Martin Wenzl <82393876+MartinWenzl99@users.noreply.github.com>
2023-11-29 12:07:53 +0100

    adding language switch function

---
+++ src/main/java/com/winewizard/winewizard/config/AppConfig.java
@@ -0,0 +1,38 @@
+package com.winewizard.winewizard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+    @Configuration
+    public class AppConfig implements WebMvcConfigurer {
+
+        @Bean
+        public LocaleResolver localeResolver() {
+            SessionLocaleResolver slr = new SessionLocaleResolver();
+            slr.setDefaultLocale(Locale.ENGLISH); // Standard-Sprache
+            return slr;
+        }
+
+        @Bean
+        public ReloadableResourceBundleMessageSource messageSource() {
+            ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+            messageSource.setBasename("classpath:messages");
+            messageSource.setDefaultEncoding("UTF-8");
+            return messageSource;
+        }
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+            localeChangeInterceptor.setParamName("lang");
+            registry.addInterceptor(localeChangeInterceptor);
+        }
+    }
---
+++ src/main/java/com/winewizard/winewizard/controller/LanguageController.java
@@ -0,0 +1,20 @@
+package com.winewizard.winewizard.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Locale;
+
+@Controller
+public class LanguageController {
+
+    @GetMapping("/changeLanguage")
+    public String changeLanguage(@RequestParam("lang") String lang, HttpServletRequest request) {
+        request.getSession().setAttribute("lang", lang);
+
+        return "redirect:/";
+    }
+}
