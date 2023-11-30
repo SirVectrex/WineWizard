c5852b4fd77d113f59b71be4f94d016541a98563
schotti1001 <schottenhammer.markus@posteo.de>
2023-11-30 10:17:22 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 -- Intial database values
--- wine(id, barcode_num, name, type, description) 
+-- wine(id, barcode_num, name, type, description)
 INSERT INTO wine (name) VALUES ('Lugana');
+
+-- address(id, city, house_number, postal_code, street)
+INSERT INTO address (postal_code, city, street, house_number) VALUES ('93333', 'Bad Goegging', 'Winestreet', 6);
+
+-- winery(id, name, address_id)
+INSERT INTO winery (name, address_id) VALUES ('Schottis Winery', 1);
