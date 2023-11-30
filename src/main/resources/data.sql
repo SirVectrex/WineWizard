3094d97e3d0070e92371cf96885f778b8017585f
schotti1001 <schottenhammer.markus@posteo.de>
2023-11-30 12:39:18 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,4 @@ INSERT INTO wine (name) VALUES ('Lugana');
 INSERT INTO address (postal_code, city, street, house_number) VALUES ('93333', 'Bad Goegging', 'Winestreet', 6);
 
 -- winery(id, name, address_id)
-INSERT INTO winery (name, address_id) VALUES ('Schottis Winery', 1);
+INSERT INTO winery (name, address_id, url_identifier) VALUES ('Schottis Winery', 1, '2520ff3c-8f72-11ee-b9d1-0242ac120002');
