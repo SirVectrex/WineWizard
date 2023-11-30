d735892a3cb93d9d09d16285f3b12403135bf5c7
schotti1001 <schottenhammer.markus@posteo.de>
2023-11-30 10:17:17 +0100

    added winery db structure

---
+++ src/main/java/com/winewizard/winewizard/model/Address.java
@@ -0,0 +1,25 @@
+package com.winewizard.winewizard.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name="address")
+public class Address implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @OneToOne(mappedBy = "address")
+    Winery winery;
+
+    String city;
+    String postalCode;
+    String street;
+    String houseNumber;
+
+}
---
+++ src/main/java/com/winewizard/winewizard/model/Winery.java
@@ -0,0 +1,22 @@
+package com.winewizard.winewizard.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name="winery")
+public class Winery implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    Address address;
+
+
+}
