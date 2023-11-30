bc24bfa0404cb8eeddcf259587ac00ffdf56ab1c
schotti1001 <schottenhammer.markus@posteo.de>
2023-11-30 12:38:41 +0100

    get+setter in winery

--- src/main/java/com/winewizard/winewizard/model/Winery.java
+++ src/main/java/com/winewizard/winewizard/model/Winery.java
@@ -1,9 +1,11 @@
 package com.winewizard.winewizard.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.UUID;
 
 @Entity
 @Table(name="winery")
@@ -11,16 +13,63 @@ public class Winery implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
 
-    String name;
+    private String name;
+
+    @GeneratedValue
+    private UUID urlIdentifier;
 
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "address_id", referencedColumnName = "id")
-    Address address;
+    private Address address;
 
     @OneToMany
-    Collection<Wine> wines;
+    private Collection<Wine> wines;
 
+    @PrePersist
+    protected void onCreate() {
+        setUrlIdentifier(java.util.UUID.randomUUID());
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getUrlIdentifier() {
+        return urlIdentifier;
+    }
+
+    public void setUrlIdentifier(UUID urlIdentifier) {
+        if(this.urlIdentifier != null){
+            System.out.println("Invalid operation: URL Identifier can not be overwritten");
+            return;
+        }
+
+        this.urlIdentifier = urlIdentifier;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Collection<Wine> getWines() {
+        return wines;
+    }
+
+    public void setWines(Collection<Wine> wines) {
+        this.wines = wines;
+    }
 }
