53e32c71ef759fc7280aea91c0d108f9f86dadc2
Martin Wenzl <82393876+MartinWenzl99@users.noreply.github.com>
2024-01-15 21:15:12 +0100

    updating newsletter mail

--- src/main/java/com/winewizard/winewizard/service/impl/EmailServiceImpl.java
+++ src/main/java/com/winewizard/winewizard/service/impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.winewizard.winewizard.service.impl;
 
 import com.winewizard.winewizard.model.RiddleResponse;
+import com.winewizard.winewizard.model.Wine;
 import com.winewizard.winewizard.repository.WineProjectionI;
 import com.winewizard.winewizard.service.EmailService;
 import jakarta.mail.MessagingException;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class EmailServiceImpl implements EmailService {
@@ -54,9 +56,53 @@ public class EmailServiceImpl implements EmailService {
             htmlBody = htmlBody.replace("riddle_placeholder", riddle.getRiddle());
             htmlBody = htmlBody.replace("answer_placeholder", riddle.getAnswer());
 
+            List<WineProjectionI> winelist = wineService.getWineRatings();
+
+            // build big String with new lines for each wine
+            String topWines = "";
+            for (WineProjectionI wine : winelist) {
+                topWines += wine.getName() + " " + wine.getAvgTasteRating() + " " + wine.getAvgDesignRating() + " " + wine.getAvgPriceRating() + "\n";
+            }
+
+            // build an html table with the wines and add a header as well pls
+            String htmltext = "<table>\n" +
+                    "  <tr>\n" +
+                    "    <th>Name</th>\n" +
+                    "    <th>Taste</th>\n" +
+                    "    <th>Design</th>\n" +
+                    "    <th>Price</th>\n" +
+                    "  </tr>\n";
+
+            for (WineProjectionI wine : winelist) {
+                htmltext += "<tr>\n" +
+                        "    <td>" + wine.getName() + "</td>\n" +
+                        "    <td>" + wine.getAvgTasteRating() + "</td>\n" +
+                        "    <td>" + wine.getAvgDesignRating() + "</td>\n" +
+                        "    <td>" + wine.getAvgPriceRating() + "</td>\n" +
+                        "  </tr>\n";
+            }
+
+            htmltext +="</table>";
+
+            htmlBody = htmlBody.replace("Top_new_wine_placeholder", htmltext);
+
+            List<Wine> allWines = wineService.getAllWines();
+
+
+            // Zufällige Index generieren
+            int randomIndex = new Random().nextInt(allWines.size());
+
+            Wine wine_of_the_week = allWines.get(randomIndex);
+
+
+
+
+            htmltext =wine_of_the_week.getName() + "\n"+ wine_of_the_week.getType() + "\n" + wine_of_the_week.getDescription();
+
+            htmlBody = htmlBody.replace("Wine_of_the_week_placeholder", htmltext);
+
             // Set the HTML content to true
             helper.setText(htmlBody, true);
-
             // Send the email
             javaMailSender.send(mimeMessage);
 
