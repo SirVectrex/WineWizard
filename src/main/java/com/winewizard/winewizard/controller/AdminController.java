8dd69745dd62750aefc2007bd602b9c04e6c0ba4
Martin Wenzl <82393876+MartinWenzl99@users.noreply.github.com>
2024-01-16 20:18:19 +0100

    implementing admin page where all users are shown divided by there role

--- src/main/java/com/winewizard/winewizard/controller/AdminController.java
+++ src/main/java/com/winewizard/winewizard/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.winewizard.winewizard.controller;
 
+import com.winewizard.winewizard.model.Role;
 import com.winewizard.winewizard.model.User;
 import com.winewizard.winewizard.repository.UserRepositoryI;
 import com.winewizard.winewizard.service.UserServiceI;
@@ -9,7 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
@@ -23,11 +27,25 @@ public class AdminController {
     @GetMapping
     public String adminPage(Model model) {
 
-        Iterable<User> user = userRepo.findAll();
+        Iterable<User> users = userRepo.findAll();
 
-        System.out.println(user.iterator().next().getLogin());
-        System.out.println(user.iterator().next().getEmail());
-        System.out.println(user.iterator().next().getRoles().get(0).getDescription());
+        // Create a map to store users based on their roles
+        Map<String, List<User>> usersByRole = new HashMap<>();
+
+        // Initialize lists for each role
+        usersByRole.put("ADMIN", new ArrayList<>());
+        usersByRole.put("wineWizard", new ArrayList<>());
+        usersByRole.put("winery", new ArrayList<>());
+
+        // Organize users into the map based on their roles
+        for (User user : users) {
+            for (Role role : user.getRoles()) {
+                String roleName = role.getDescription();
+                usersByRole.get(roleName).add(user);
+            }
+        }
+
+        model.addAttribute("usersByRole", usersByRole);
 
         return "general/user-info";
     }
