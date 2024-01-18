a10329d72631fb5ad25df1146da558c906727af3
schotti1001 <schottenhammer.markus@posteo.de>
2024-01-18 12:48:51 +0100

    removed address from winery

--- src/main/java/com/winewizard/winewizard/model/Address.java
+++ src/main/java/com/winewizard/winewizard/model/Address.java
@@ -8,14 +8,14 @@ import java.util.Collection;
 @Entity
 @Table(name="address")
 public class Address implements Serializable {
-
+//TODO: is this class required?
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     Long id;
 
-    @OneToOne(mappedBy = "address")
-    Winery winery;
+  //  @OneToOne(mappedBy = "address")
+   // Winery winery;
 
     String city;
     String postalCode;
