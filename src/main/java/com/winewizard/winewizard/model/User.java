fce7f4e9b779839c53fb7845be9658e509ad7be4
schotti1001 <schottenhammer.markus@posteo.de>
2024-01-20 11:32:11 +0100

    used getter attribute

--- src/main/java/com/winewizard/winewizard/model/User.java
+++ src/main/java/com/winewizard/winewizard/model/User.java
@@ -27,38 +27,49 @@ public class User implements Serializable, UserDetails {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	@Getter
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	Long id;
+	@Getter
 	@NotBlank(message = "username is mandatory")
 	//TODO: check if contains blank
 	@Size(min = 5, max = 50, message ="Username must have at least 5 characters")
 	@Column(unique = true)
 	private String login;
+	@Getter
 	@Transient
 	private String zipCodeInput;
+	@Getter
 	@ManyToOne
 	@JoinColumn(name="zip_code", nullable = false)
 	private ZipCode zipCode;
 	@NotBlank(message = "password is mandatory")
 	private String password;
+	@Getter
 	@Transient
 	private String passwordRepeat;
+	@Getter
 	@NotBlank(message = "Email is mandatory")
 	//TODO: unique and check on sign in
 	private String email;
+	@Getter
 	@NotBlank(message = "Phone Number is mandatory")
 	//TODO: check validity
 	private String phone;
+	@Getter
 	@Transient
 	private boolean olderThanSixteen;
+	@Getter
 	@Transient
 	private boolean wineryUser;
+	@Getter
 	private boolean active = true;
 
 	@Transient //TODO: annotation übeprüfen
 	private List<GrantedAuthority> authorities;
 	
+	@Getter
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name="userrole",
@@ -66,9 +77,12 @@ public class User implements Serializable, UserDetails {
 			inverseJoinColumns = @JoinColumn(name="idrole")
 			)
 	private List<Role> roles = new ArrayList<Role>();
+	@Getter
 	@Column()
 	private String verificationCode ;
+	@Getter
 	private boolean verified;
+	@Getter
 	private String personalProfileId ;
 
 	public User(User user) {
@@ -106,32 +120,16 @@ public class User implements Serializable, UserDetails {
 	}
 
 
-	public String getVerificationCode() {
-		return verificationCode;
-	}
-
 	public void setVerificationCode(String verificationCode) {
 		this.verificationCode = verificationCode;
-	}
-
-	public boolean isVerified() {
-		return verified;
 	}
 
 	public void setVerified(boolean verified) {
 		this.verified = verified;
 	}
 
-	public List<Role> getRoles() {
-		return roles;
-	}
-
 	public void setRoles(List<Role> roles) {
 		this.roles = roles;
-	}
-
-	public String getPersonalProfileId() {
-		return personalProfileId;
 	}
 
 	public void setPersonalProfileId(String personalProfileId) {
@@ -142,42 +140,10 @@ public class User implements Serializable, UserDetails {
 		return "http://localhost:8080/profile/" + getPersonalProfileId();
 	}
 
-	public boolean isWineryUser() {
-		return wineryUser;
-	}
-
 	public void setWineryUser(boolean wineryUser) {
 		this.wineryUser = wineryUser;
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPasswordRepeat() {
-		return passwordRepeat;
-	}
-
-	public void setPasswordRepeat(String passwordRepeat) {
-		this.passwordRepeat = passwordRepeat;
-	}
-
-	public String getZipCodeInput() {
-		return zipCodeInput;
-	}
-
-	public void setZipCodeInput(String zipCode) {
-		this.zipCodeInput = zipCode;
-	}
-
-	public ZipCode getZipCode() {
-		return zipCode;
-	}
-
+	
 	public void setZipCode(ZipCode zipCode) {
 		this.zipCode = zipCode;
 	}
@@ -186,40 +152,8 @@ public class User implements Serializable, UserDetails {
 		this.email = email;
 	}
 
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public boolean isOlderThanSixteen() {
-		return olderThanSixteen;
-	}
-
-	public void setOlderThanSixteen(boolean olderThanSixteen) {
-		this.olderThanSixteen = olderThanSixteen;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
-
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getLogin() {
-		return login;
 	}
 
 	public void setLogin(String login) {
