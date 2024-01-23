fac693ef632302497bab0ce0befa9b1a55a28888
schotti1001 <schottenhammer.markus@posteo.de>
2024-01-23 10:22:48 +0100

    added id to wineprojection

--- src/main/java/com/winewizard/winewizard/repository/WineProjectionI.java
+++ src/main/java/com/winewizard/winewizard/repository/WineProjectionI.java
@@ -3,6 +3,7 @@ package com.winewizard.winewizard.repository;
 public interface WineProjectionI {
     // projection interface tof pulling wine information from the database
     String getName();
+    String getId();
     Double getAvgTasteRating();
     Double getAvgDesignRating();
     Double getAvgPriceRating();
--- src/main/java/com/winewizard/winewizard/repository/impl/WineRepositoryImpl.java
+++ src/main/java/com/winewizard/winewizard/repository/impl/WineRepositoryImpl.java
@@ -16,29 +16,36 @@ import java.util.List;
 @Repository
 public interface WineRepositoryImpl extends WineRepositoryI {
 
-    @Query("SELECT w.name as name, ROUND(AVG(r.ratingTaste), 2) as avgTasteRating, ROUND(AVG(r.ratingDesign), 2) as avgDesignRating, ROUND(AVG(r.ratingPrice), 2) as avgPriceRating " +
+    @Query("SELECT w.name as name,  w.id as id, ROUND(AVG(r.ratingTaste), 2) as avgTasteRating, ROUND(AVG(r.ratingDesign), 2) as avgDesignRating, ROUND(AVG(r.ratingPrice), 2) as avgPriceRating " +
             "FROM Wine w JOIN Rating r ON w = r.wine " +
             "GROUP BY w.id, w.name " +
             "ORDER BY AVG(r.ratingTaste) DESC")
     List<WineProjectionI> findWinesWithAverageRatings();
 
-    @Query("SELECT w.name as name, ROUND(AVG(r.ratingTaste), 2) as avgTasteRating, ROUND(AVG(r.ratingDesign), 2) as avgDesignRating, ROUND(AVG(r.ratingPrice), 2) as avgPriceRating " +
+    @Query("SELECT w.name as name,  w.id as id, ROUND(AVG(r.ratingTaste), 2) as avgTasteRating, ROUND(AVG(r.ratingDesign), 2) as avgDesignRating, ROUND(AVG(r.ratingPrice), 2) as avgPriceRating " +
             "FROM Wine w JOIN Rating r ON w = r.wine " +
             "GROUP BY w.id, w.name " +
             "ORDER BY AVG(r.ratingTaste) DESC")
     Page<WineProjectionI> findWinesWithAverageRatings_Pages(Pageable pageable);
 
+    @Query("SELECT w.name as name,  w.id as id, ROUND(AVG(r.ratingTaste), 2) as avgTasteRating, ROUND(AVG(r.ratingDesign), 2) as avgDesignRating, ROUND(AVG(r.ratingPrice), 2) as avgPriceRating " +
+            "FROM Wine w JOIN Rating r ON w = r.wine " +
+            "WHERE w.winery.id = :winery_id " +
+            "GROUP BY w.id, w.name " +
+            "ORDER BY AVG(r.ratingTaste) DESC")
+    Page<WineProjectionI> findWinesWithAverageRatingsByWinery_Pages(Pageable pageable, @Param("winery_id") long winery_id);
+
     @Query(value = "Select (Select Count(*) from RATING) as numratings, (Select Count(*) from WINE) as numwines,  (Select Count(*) from WINERY) as numwinery, (Select Count(*) from \"USER\") as numuser", nativeQuery = true)
     List<StatsProjectionI> getStats();
 
-    @Query(value = "SELECT w.NAME as name, ROUND(AVG(r.TASTE_RATING), 2) as avgTasteRating, ROUND(AVG(r.DESIGN_RATING), 2) as avgDesignRating, ROUND(AVG(r.PRICE_RATING), 2) as avgPriceRating " +
+    @Query(value = "SELECT w.NAME as name,  w.id as id, ROUND(AVG(r.TASTE_RATING), 2) as avgTasteRating, ROUND(AVG(r.DESIGN_RATING), 2) as avgDesignRating, ROUND(AVG(r.PRICE_RATING), 2) as avgPriceRating " +
             "FROM \"USER\" u INNER JOIN RATING r on u.ID = r.USER_ID " +
             "INNER JOIN WINE w ON r.WINE_ID = w.ID WHERE u.ZIP_CODE = 93333 " +
             "GROUP BY w.NAME " +
             "ORDER BY AVG(r.TASTE_RATING) DESC", nativeQuery = true)
     List<WineProjectionI> getWinesByZipCode();
 
-    @Query(value = "SELECT w.NAME as name, ROUND(AVG(r.TASTE_RATING), 2) as avgTasteRating, ROUND(AVG(r.DESIGN_RATING), 2) as avgDesignRating, ROUND(AVG(r.PRICE_RATING), 2) as avgPriceRating " +
+    @Query(value = "SELECT w.NAME as name,  w.id as id, ROUND(AVG(r.TASTE_RATING), 2) as avgTasteRating, ROUND(AVG(r.DESIGN_RATING), 2) as avgDesignRating, ROUND(AVG(r.PRICE_RATING), 2) as avgPriceRating " +
             "FROM \"USER\" u INNER JOIN RATING r on u.ID = r.USER_ID " +
             "INNER JOIN WINE w ON r.WINE_ID = w.ID WHERE u.ZIP_CODE = :zipcode " +
             "GROUP BY w.NAME " +
